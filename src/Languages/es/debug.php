ae0b306264d0c36f13cf11c30b5dae622f9709e5
Natan Felles <natanfelles@gmail.com>
2019-11-01 20:25:47 -0300

    Add spanish translation

---
+++ src/Languages/es/debug.php
@@ -0,0 +1,10 @@
+<?php
+return [
+	'exception' => 'Excepción',
+	'message' => 'Mensaje',
+	'file' => 'Archivo',
+	'line' => 'Línea',
+	'trace' => 'Trace',
+	'exceptionTitle' => 'Error 500 - Error interno del servidor',
+	'exceptionDescription' => 'Algo está mal. Por favor vuelve más tarde.',
+];
