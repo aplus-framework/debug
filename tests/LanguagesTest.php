4c0f7bb9ddb72be31868ab601cf118ddd5d061d2
Natan Felles <natanfelles@gmail.com>
2021-04-19 15:08:24 -0300

    Add LanguagesTest

---
+++ tests/LanguagesTest.php
@@ -0,0 +1,31 @@
+<?php namespace Tests\Debug;
+
+use PHPUnit\Framework\TestCase;
+
+class LanguagesTest extends TestCase
+{
+	protected string $langDir = __DIR__ . '/../src/Languages/';
+
+	protected function getCodes()
+	{
+		$codes = \array_filter(\glob($this->langDir . '*'), 'is_dir');
+		$length = \strlen($this->langDir);
+		foreach ($codes as &$dir) {
+			$dir = \substr($dir, $length);
+		}
+		return $codes;
+	}
+
+	public function testKeys()
+	{
+		$rules = require $this->langDir . 'en/debug.php';
+		$rules = \array_keys($rules);
+		\sort($rules);
+		foreach ($this->getCodes() as $code) {
+			$lines = require $this->langDir . $code . '/debug.php';
+			$lines = \array_keys($lines);
+			\sort($lines);
+			$this->assertEquals($rules, $lines, 'Language: ' . $code);
+		}
+	}
+}
