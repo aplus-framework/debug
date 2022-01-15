bb9d076c17402ebf22f841f5a3253ad44141387a
Natan Felles <natanfelles@gmail.com>
2022-01-15 02:38:24 -0300

    Update exception views

--- src/Views/development.php
+++ src/Views/development.php
@@ -8,168 +8,176 @@
 <html lang="<?= $handler->getLanguage()->getCurrentLocale() ?>" dir="<?= $handler->getLanguage()
     ->getCurrentLocaleDirection() ?>">
 <head>
-	<meta charset="utf-8">
-	<title><?= $handler->getLanguage()->render('debug', 'exception') ?>: <?=
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?= $handler->getLanguage()->render('debug', 'exception') ?>: <?=
         htmlentities($exception->getMessage()) ?></title>
-	<style>
-		body {
-			margin: 0 0 20px;
-			font-family: sans-serif;
-			background-color: whitesmoke;
-		}
+    <style>
+        body {
+            margin: 0 0 20px;
+            font-family: sans-serif;
+            background-color: whitesmoke;
+        }
 
-		header {
-			background: red;
-			color: white;
-			border: solid black;
-			border-width: 1px 0;
-			padding: 20px 20px;
-		}
+        header {
+            background: red;
+            color: white;
+            border: solid black;
+            border-width: 1px 0;
+            padding: 20px 20px;
+        }
 
-		h1, h2, h3 {
-			margin: 0 0 5px;
-		}
+        h1, h2, h3 {
+            margin: 0 0 5px;
+        }
 
-		section {
-			padding: 10px 20px 5px;
-			box-shadow: inset 2px 2px 2px rgba(0, 0, 0, .25);
-			border-bottom: 1px solid black;
-		}
+        section {
+            padding: 10px 20px 5px;
+            border-bottom: 1px solid black;
+        }
 
-		.file {
-			background: darkgray;
-		}
+        section .header {
+            background: red;
+            color: #fff;
+            font-weight: bold;
+            border: 1px solid #000;
+            padding: 10px;
+        }
 
-		.file div {
-			display: inline-block;
-		}
+        .file {
+            background: darkgray;
+        }
 
-		.trace {
-			background-color: lightgray;
-		}
+        .file div {
+            display: inline-block;
+        }
 
-		.log {
-			background-color: white;
-		}
+        .trace {
+            background-color: lightgray;
+        }
 
-		dl {
-			font-family: monospace;
-			font-size: 14px;
-			margin: 5px 0 10px;
-			border: 1px solid black;
-		}
+        .log {
+            background-color: white;
+        }
 
-		dt {
-			border-bottom: 1px solid black;
-			background: black;
-			color: white;
-			padding: 1%;
-			width: 98%;
-		}
+        dl {
+            font-family: monospace;
+            font-size: 14px;
+            margin: 5px 0 10px;
+            border: 1px solid black;
+        }
 
-		dd {
-			background: whitesmoke;
-			margin: 0;
-			overflow-x: auto;
-		}
+        dt {
+            border-bottom: 1px solid black;
+            background: black;
+            color: white;
+            padding: 1%;
+            width: 98%;
+        }
 
-		pre.code {
-			line-height: 1.2rem;
-			display: inline-block;
-			width: 80%;
-			margin: 0;
-			padding: 5px;
-			float: left;
-		}
+        dd {
+            background: whitesmoke;
+            margin: 0;
+            overflow-x: auto;
+        }
 
-		dd div {
-			min-width: 25px;
-			display: inline-block;
-			line-height: 1.2rem;
-			white-space: pre;
-			text-align: right;
-			padding: 5px;
-			float: left;
-			background: #fff;
-			border-right: 1px #ddd solid;
-		}
+        pre.code {
+            line-height: 1.2rem;
+            display: inline-block;
+            width: 80%;
+            margin: 0;
+            padding: 5px;
+            float: left;
+        }
 
-		dd div span {
-			color: red;
-		}
+        dd div {
+            min-width: 25px;
+            display: inline-block;
+            line-height: 1.2rem;
+            white-space: pre;
+            text-align: right;
+            padding: 5px;
+            float: left;
+            background: #fff;
+            border-right: 1px #ddd solid;
+        }
 
-		dt span {
-			background: red;
-			padding: 2px 6px;
-		}
+        dd div span {
+            color: red;
+        }
 
-		.input,
-		.log {
-			border: 0;
-		}
+        dt span {
+            background: red;
+            padding: 2px 6px;
+        }
 
-		.log {
-			border-top: 1px solid black;
-		}
+        .input,
+        .log {
+            border: 0;
+        }
 
-		table {
-			border: 1px black solid;
-			border-spacing: 0;
-			width: 100%;
-			margin: 5px 0 10px;
-		}
+        .log {
+            border-top: 1px solid black;
+        }
 
-		.log table {
-			border-top: 0;
-		}
+        table {
+            border: 1px black solid;
+            border-spacing: 0;
+            width: 100%;
+            margin: 5px 0 10px;
+        }
 
-		th {
-			border-right: 1px black solid;
-		}
+        .log table {
+            border-top: 0;
+        }
 
-		th, td {
-			border-top: 1px black solid;
-			padding: 3px;
-		}
+        th {
+            border-right: 1px black solid;
+        }
 
-		tr:hover {
-			background: lightgray;
-		}
+        th, td {
+            border-top: 1px black solid;
+            padding: 3px;
+        }
 
-		thead th {
-			text-align: left;
-			background: black;
-			color: white;
-			font-size: 110%;
-		}
+        tr:hover {
+            background: lightgray;
+        }
 
-		tbody th {
-			text-align: right;
-			background: darkgray;
-			min-width: 40%;
-		}
-	</style>
+        thead th {
+            text-align: left;
+            background: black;
+            color: white;
+            font-size: 110%;
+        }
+
+        tbody th {
+            text-align: right;
+            background: darkgray;
+            min-width: 40%;
+        }
+    </style>
 </head>
 <body>
 <header>
-	<small><?= $handler->getLanguage()->render('debug', 'exception') ?>:</small>
-	<h1><?= $exception::class ?></h1>
-	<small><?= $handler->getLanguage()->render('debug', 'message') ?>:</small>
-	<h2><?= htmlentities($exception->getMessage()) ?></h2>
+    <small><?= $handler->getLanguage()->render('debug', 'exception') ?>:</small>
+    <h1><?= $exception::class ?></h1>
+    <small><?= $handler->getLanguage()->render('debug', 'message') ?>:</small>
+    <h2><?= htmlentities($exception->getMessage()) ?></h2>
 </header>
 <section class="file">
-	<div>
-		<small><?= $handler->getLanguage()->render('debug', 'file') ?>:</small>
-		<h3><?= htmlentities($exception->getFile()) ?></h3>
-	</div>
-	<div>
-		<small><?= $handler->getLanguage()->render('debug', 'line') ?>:</small>
-		<h3><?= $exception->getLine() ?></h3>
-	</div>
+    <div>
+        <small><?= $handler->getLanguage()->render('debug', 'file') ?>:</small>
+        <h3><?= htmlentities($exception->getFile()) ?></h3>
+    </div>
+    <div>
+        <small><?= $handler->getLanguage()->render('debug', 'line') ?>:</small>
+        <h3><?= $exception->getLine() ?></h3>
+    </div>
 </section>
 <section class="trace">
-	<small><?= $handler->getLanguage()->render('debug', 'trace') ?>:</small>
-	<?php
+    <div class="header"><?= $handler->getLanguage()->render('debug', 'trace') ?>:</div>
+    <?php
     $traces = $exception->getTrace();
     if ($traces
         && isset($traces[0]['file'])
@@ -184,16 +192,16 @@
         $traces = array_reverse($traces);
     }
     ?>
-	<?php foreach ($traces as $key => $trace) : ?>
-		<?php if (isset($trace['file'])) : ?>
-			<?php if (is_readable($trace['file'])) : ?>
-				<dl>
-					<dt>
-						<span><?= count($traces) - $key ?></span>
-						<?= $trace['file'] ?>:<?= $trace['line'] ?>
-					</dt>
-					<dd>
-						<?php
+    <?php foreach ($traces as $key => $trace) : ?>
+        <?php if (isset($trace['file'])) : ?>
+            <?php if (is_readable($trace['file'])) : ?>
+                <dl>
+                    <dt>
+                        <span><?= count($traces) - $key ?></span>
+                        <?= $trace['file'] ?>:<?= $trace['line'] ?>
+                    </dt>
+                    <dd>
+                        <?php
                         $lines = [];
                         $pre = '';
                         $handle = fopen($trace['file'], 'rb');
@@ -210,7 +218,7 @@
                             fclose($handle);
                         }
                         ?>
-						<div><?php
+                        <div><?php
                             foreach ($lines as $line) {
                                 if ($line === $trace['line']) {
                                     echo '<span>';
@@ -221,23 +229,23 @@
                                 }
                             }
                             ?></div>
-						<pre class="code"><?= htmlentities($pre) ?></pre>
-					</dd>
-				</dl>
-			<?php else : ?>
-				<dl>
-					<dt>
-						<span><?= $key ?></span> File
-						<em><?= $trace['file'] ?></em> is not readable.
-					</dt>
-				</dl>
-			<?php endif ?>
-		<?php endif ?>
-	<?php endforeach ?>
+                        <pre class="code"><?= htmlentities($pre) ?></pre>
+                    </dd>
+                </dl>
+            <?php else : ?>
+                <dl>
+                    <dt>
+                        <span><?= $key ?></span> File
+                        <em><?= $trace['file'] ?></em> is not readable.
+                    </dt>
+                </dl>
+            <?php endif ?>
+        <?php endif ?>
+    <?php endforeach ?>
 </section>
 <section class="input">
-	<small>Input:</small>
-	<?php
+    <div class="header">Input:</div>
+    <?php
     $input = [
         'ENV' => filter_input_array(\INPUT_ENV) ?: [],
         'SERVER' => filter_input_array(\INPUT_SERVER) ?: [],
@@ -251,63 +259,63 @@
     unset($item);
     ?>
 
-	<?php foreach ($input as $key => $values) : ?>
-		<?php
+    <?php foreach ($input as $key => $values) : ?>
+        <?php
         if (empty($values)) {
             continue;
         }
         ?>
-		<table>
-			<thead>
-			<tr>
-				<th colspan="2"><?= $key ?></th>
-			</tr>
-			</thead>
-			<tbody>
-			<?php foreach ($values as $field => $value) : ?>
-				<tr>
-					<th><?= htmlentities($field) ?></th>
-					<td><?= htmlentities(is_array($value) ? print_r($value, true) : $value) ?></td>
-				</tr>
-			<?php endforeach ?>
-			</tbody>
-		</table>
-	<?php endforeach ?>
+        <table>
+            <thead>
+            <tr>
+                <th colspan="2"><?= $key ?></th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($values as $field => $value) : ?>
+                <tr>
+                    <th><?= htmlentities($field) ?></th>
+                    <td><?= htmlentities(is_array($value) ? print_r($value, true) : $value) ?></td>
+                </tr>
+            <?php endforeach ?>
+            </tbody>
+        </table>
+    <?php endforeach ?>
 </section>
 <?php if ($handler->getLogger() && ($log = $handler->getLogger()->getLastLog())): ?>
-	<section class="log">
-		<small>Log:</small>
-		<table>
-			<tr>
-				<th>Filename</th>
-				<td><?= htmlentities($log->filename) ?></td>
-			</tr>
-			<tr>
-				<th>Date</th>
-				<td><?= htmlentities($log->date) ?></td>
-			</tr>
-			<tr>
-				<th>Time</th>
-				<td><?= htmlentities($log->time) ?></td>
-			</tr>
-			<tr>
-				<th>Level Name</th>
-				<td><?= htmlentities($log->levelName) ?></td>
-			</tr>
-			<tr>
-				<th>ID</th>
-				<td><?= htmlentities($log->id) ?></td>
-			</tr>
-			<tr>
-				<th>Message</th>
-				<td style="white-space: pre-wrap"><?= htmlentities($log->message) ?></td>
-			</tr>
-			<tr>
-				<th>Written</th>
-				<td><?= $log->written ? 'true' : 'false' ?></td>
-			</tr>
-		</table>
-	</section>
+    <section class="log">
+        <div class="header">Log:</div>
+        <table>
+            <tr>
+                <th>Filename</th>
+                <td><?= htmlentities($log->filename) ?></td>
+            </tr>
+            <tr>
+                <th>Date</th>
+                <td><?= htmlentities($log->date) ?></td>
+            </tr>
+            <tr>
+                <th>Time</th>
+                <td><?= htmlentities($log->time) ?></td>
+            </tr>
+            <tr>
+                <th>Level Name</th>
+                <td><?= htmlentities($log->levelName) ?></td>
+            </tr>
+            <tr>
+                <th>ID</th>
+                <td><?= htmlentities($log->id) ?></td>
+            </tr>
+            <tr>
+                <th>Message</th>
+                <td style="white-space: pre-wrap"><?= htmlentities($log->message) ?></td>
+            </tr>
+            <tr>
+                <th>Written</th>
+                <td><?= $log->written ? 'true' : 'false' ?></td>
+            </tr>
+        </table>
+    </section>
 <?php endif ?>
 </body>
 </html>
--- src/Views/production.php
+++ src/Views/production.php
@@ -7,21 +7,29 @@
 <html lang="<?= $handler->getLanguage()->getCurrentLocale() ?>" dir="<?= $handler->getLanguage()
     ->getCurrentLocaleDirection() ?>">
 <head>
-	<meta charset="utf-8">
-	<title><?= $handler->getLanguage()->render('debug', 'exceptionTitle') ?></title>
-	<style>
-		body {
-			font-family: monospace, sans-serif;
-			background: black;
-			color: white;
-		}
-	</style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?= $handler->getLanguage()->render('debug', 'exceptionTitle') ?></title>
+    <style>
+        body {
+            background: #000;
+            color: #eee;
+            font-family: monospace, sans-serif;
+            font-size: 1rem;
+            line-height: 1.5rem;
+            margin: 1rem;
+        }
+
+        h1 {
+            color: #fff;
+        }
+    </style>
 </head>
 <body>
 <h1><?= $handler->getLanguage()->render('debug', 'exceptionTitle') ?></h1>
 <p><?= $handler->getLanguage()->render('debug', 'exceptionDescription') ?></p>
 <?php if ($handler->getLogger() && $handler->getLogger()->getLastLog()?->written): ?>
-	<p>Log Id: <?= $handler->getLogger()->getLastLog()->id ?></p>
+    <p>Log Id: <?= $handler->getLogger()->getLastLog()->id ?></p>
 <?php endif ?>
 </body>
 </html>
