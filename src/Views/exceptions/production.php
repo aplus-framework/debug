a3357c90ec763c774edc0c4ee57be20eceadf3a9
Natan Felles <natanfelles@gmail.com>
2025-06-19 21:10:42 -0300

    Set Language instance in a variable

--- src/Views/exceptions/production.php
+++ src/Views/exceptions/production.php
@@ -2,15 +2,15 @@
 /**
  * @var Framework\Debug\ExceptionHandler $handler
  */
+$lang = $handler->getLanguage();
 ?>
 <!doctype html>
-<html lang="<?= $handler->getLanguage()->getCurrentLocale() ?>" dir="<?= $handler->getLanguage()
-    ->getCurrentLocaleDirection() ?>">
+<html lang="<?= $lang->getCurrentLocale() ?>" dir="<?= $lang->getCurrentLocaleDirection() ?>">
 <head>
     <meta charset="utf-8">
     <meta name="robots" content="noindex">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?= $handler->getLanguage()->render('debug', 'exceptionTitle') ?></title>
+    <title><?= $lang->render('debug', 'exceptionTitle') ?></title>
     <link rel="shortcut icon" href="data:image/png;base64,<?= base64_encode((string) file_get_contents(__DIR__ . '/favicons/production.png')) ?>">
     <style>
         body {
@@ -35,23 +35,22 @@
     </style>
 </head>
 <body>
-<h1><?= $handler->getLanguage()->render('debug', 'exceptionTitle') ?></h1>
-<p><?= $handler->getLanguage()->render('debug', 'exceptionDescription') ?></p>
+<h1><?= $lang->render('debug', 'exceptionTitle') ?></h1>
+<p><?= $lang->render('debug', 'exceptionDescription') ?></p>
 
 <?php
 if ($handler->isShowingLogId()) :
     $log = $handler->getLogger()?->getLastLog();
     if ($log):
         ?>
-        <p>Log Id: <span class="log-id" title="<?= $handler->getLanguage()
-                ->render('debug', 'clickToCopyLogId') ?>">
+        <p>Log Id: <span class="log-id" title="<?= $lang->render('debug', 'clickToCopyLogId') ?>">
                 <?= htmlentities($log->id) ?>
             </span>
         </p>
         <script>
             document.querySelector('.log-id').onclick = function () {
                 navigator.clipboard.writeText(this.innerText);
-                alert("<?= $handler->getLanguage()->render('debug', 'logIdCopied') ?>");
+                alert("<?= $lang->render('debug', 'logIdCopied') ?>");
             }
         </script>
     <?php
